896448b976aa8399a53fc0e7cc4ca9871442d9b2
Edward Chan <edwardchan1350@gmail.com>
2019-10-15 08:25:08 -0400

    Update location on map.

--- js/scripts.js
+++ js/scripts.js
@@ -231,7 +231,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 40.7163146, lng: -73.6310198};
+    var itc_kol = {lat: 40.716916, lng: -73.628820};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
@@ -245,7 +245,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 40.7163146, lng: -73.6310198};
+    var la_fiesta = {lat: 40.716916, lng: -73.628820};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
